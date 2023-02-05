4ce4030acf1bbf8685706eeffc891830cc112e6b
Mohit Kumar <kvnl.mohitchanchlani@gmail.com>
2023-02-05 23:44:18 +0530

    Changes made to AccountCreateActivity.kt

--- app/src/main/java/co/mohit/credock/View/AccountCreateActivity.kt
+++ app/src/main/java/co/mohit/credock/View/AccountCreateActivity.kt
@@ -1,25 +1,49 @@
 package co.mohit.credock.View
 
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import co.mohit.credock.R
+import kotlinx.android.synthetic.main.activity_account_create.*
+import kotlinx.android.synthetic.main.activity_auth.*
 
 class AccountCreateActivity : AppCompatActivity() {
 
-    val userProfileFragment = findViewById<View>(R.id.fragment_userProfileDetails)
-    val userPinFragment = findViewById<View>(R.id.fragment_userPinDetails)
-    val fragmentMgr = getSupportFragmentManager()
+    val userProfileFrag = UserProfileDetails()
+    val userPinFrag = UserPinSetup()
+    val fragmentMgr = supportFragmentManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_create)
 
+        initializeFragments(userProfileFrag)
+
+    }
+
+    private fun initializeFragments(fragment:Fragment) {
+        val fragmentTransaction = fragmentMgr.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_userDetials,fragment)
+        fragmentTransaction.commit()
     }
 
     override fun onResume() {
         super.onResume()
 
+        btn_next.setOnClickListener(View.OnClickListener {
+            btn_next.visibility = View.GONE
+            btn_previous.visibility = View.VISIBLE
+            btn_userDetialSubmit.visibility = View.VISIBLE
+            initializeFragments(userPinFrag)
+        })
+
+        btn_previous.setOnClickListener(View.OnClickListener {
+            btn_next.visibility = View.VISIBLE
+            btn_previous.visibility = View.GONE
+            btn_userDetialSubmit.visibility = View.GONE
+            initializeFragments(userProfileFrag)
+        })
     }
 }
