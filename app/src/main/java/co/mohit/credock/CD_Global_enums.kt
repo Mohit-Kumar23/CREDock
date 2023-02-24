884298f4e5b61410b4b50e676c910ba10bad89e9
Mohit Kumar <kvnl.mohitchanchlani@gmail.com>
2023-02-24 22:10:54 +0530

    Global Enum file is added

---
+++ app/src/main/java/co/mohit/credock/CD_Global_enums.kt
@@ -0,0 +1,7 @@
+package co.mohit.credock
+
+enum class CD_Global_enums(val value:Int) {
+    XX_ERROR(-1),
+    XX_NOTFOUND(0),
+    XX_OK(1)
+}
--- app/src/main/java/co/mohit/credock/View/UserPinSetup.kt
+++ app/src/main/java/co/mohit/credock/View/UserPinSetup.kt
@@ -1,11 +1,17 @@
 package co.mohit.credock.View
 
 import android.os.Bundle
+import android.os.TestLooperManager
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnKeyListener
 import android.view.ViewGroup
 import co.mohit.credock.R
+import kotlinx.android.synthetic.main.fragment_user_pin_setup.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,7 +28,9 @@ class UserPinSetup : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private var keyEventCode:Int? = null
+
+   override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
@@ -37,6 +45,133 @@ class UserPinSetup : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_user_pin_setup, container, false)
     }
+
+    override fun onResume() {
+        super.onResume()
+        et_newPinDigit1.addTextChangedListener(object: TextWatcher
+        {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(!et_newPinDigit1.text.isNullOrEmpty())
+                    et_newPinDigit2.requestFocus()
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        et_newPinDigit2.addTextChangedListener(object: TextWatcher
+        {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(!et_newPinDigit2.text.isNullOrEmpty())
+                    et_newPinDigit3.requestFocus()
+                else
+                    et_newPinDigit1.requestFocus()
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        et_newPinDigit3.addTextChangedListener(object:TextWatcher
+        {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(!et_newPinDigit3.text.isNullOrEmpty())
+                    et_newPinDigit4.requestFocus()
+                else
+                    et_newPinDigit2.requestFocus()
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        et_newPinDigit4.addTextChangedListener(object:TextWatcher
+        {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(et_newPinDigit4.text.isNullOrEmpty())
+                    et_newPinDigit3.requestFocus()
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        et_reEnterPinDigit1.addTextChangedListener(object:TextWatcher
+        {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(!et_reEnterPinDigit1.text.isNullOrEmpty())
+                    et_reEnterPinDigit2.requestFocus()
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        })
+        et_reEnterPinDigit2.addTextChangedListener(object:TextWatcher
+        {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(!et_reEnterPinDigit2.text.isNullOrEmpty())
+                    et_reEnterPinDigit3.requestFocus()
+                else
+                    et_reEnterPinDigit1.requestFocus()
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        })
+        et_reEnterPinDigit3.addTextChangedListener(object :TextWatcher
+        {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(!et_reEnterPinDigit3.text.isNullOrEmpty())
+                    et_reEnterPinDigit4.requestFocus()
+                else
+                    et_reEnterPinDigit2.requestFocus()
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        })
+        et_reEnterPinDigit4.addTextChangedListener(object :TextWatcher
+        {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(et_reEnterPinDigit4.text.isNullOrEmpty())
+                    et_reEnterPinDigit3.requestFocus()
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        et_otpDigit1.addTextChangedListener(object :TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(!et_otpDigit1.text.isNullOrEmpty())
+                    et_otpDigit2.requestFocus()
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        })
+        et_otpDigit2.addTextChangedListener(object :TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(!et_otpDigit2.text.isNullOrEmpty())
+                    et_otpDigit3.requestFocus()
+                else
+                    et_otpDigit1.requestFocus()
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        })
+        et_otpDigit3.addTextChangedListener(object :TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(!et_otpDigit3.text.isNullOrEmpty())
+                     et_otpDigit4.requestFocus()
+                else
+                    et_otpDigit2.requestFocus()
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        })
+        et_otpDigit4.addTextChangedListener(object :TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(et_otpDigit4.text.isNullOrEmpty())
+                    et_otpDigit3.requestFocus()
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+    }
+
 
     companion object {
         /**
